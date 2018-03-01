dec3166252bd4d41c9975c7c92ee19551fcd0161
Sander de Jong <sanderdejong@outlook.com>
2018-03-01 23:51:14 +0100

    Add: output files

--- src/main/java/teampalooza/App.java
+++ src/main/java/teampalooza/App.java
@@ -4,17 +4,13 @@ public class App {
 
     public static void main(String[] args) {
         CityMap cityMap = new CityMap();
-        cityMap.loadMapFromFile("a_example.in");
+        cityMap.loadMapFromFile("c_no_hurry.in");
 
         Simulation simulation = new Simulation(cityMap);
         simulation.start();
 
         simulation.printOutput();
     }
-
-
-
-
 
 
 }
