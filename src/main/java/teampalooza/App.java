f8ca4a647f81fdc004a8548bc5be2d7a3e994475
susmitadumirai <09srai4@gmail.com>
2018-03-01 22:55:52 +0000

    Updated change of b.out

--- src/main/java/teampalooza/App.java
+++ src/main/java/teampalooza/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         CityMap cityMap = new CityMap();
-        cityMap.loadMapFromFile("c_no_hurry.in");
+        cityMap.loadMapFromFile("b_should_be_easy.in");
 
         Simulation simulation = new Simulation(cityMap);
         simulation.start();
