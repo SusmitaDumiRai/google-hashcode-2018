a8bbaddbd6df093b6c2f52f19985387b9ca63975
Sander de Jong <sanderdejong@outlook.com>
2018-03-01 22:13:19 +0100

    Add: Simulation set up

---
+++ src/main/java/teampalooza/Simulation.java
@@ -0,0 +1,45 @@
+package teampalooza;
+
+public class Simulation {
+    private CityMap cityMap;
+
+    public Simulation(CityMap cityMap) {
+        this.cityMap = cityMap;
+    }
+
+    public void start() {
+        for (int i = 0; i < cityMap.getSteps(); i++) {
+            assignRides();
+            for (Car c : this.cityMap.getCarList()) {
+                if (c.getCurrentRide().getEarliestStartStep() >= i) {
+                    if (c.getJourneyTime() != 0) {
+                        c.setJourneyTime();
+                    } else {
+                        c.setOccupied(false);
+                        c.getCurrentRide().setCompleted(true);
+                    }
+                }
+            }
+            assignRides();
+        }
+    }
+
+    private void assignRides() {
+        for (Car c : this.cityMap.getCarList()) {
+            for (Ride r : this.cityMap.getRideList()) {
+                if (!r.isAssigned() && !c.isOccupied()) {
+                    c.setCurrentRide(r);
+                    c.setAmountOfRides();
+                    r.setAssigned(true);
+                    c.setOccupied(true);
+                }
+            }
+        }
+    }
+
+    public void printOutput() {
+        for (Car c : this.cityMap.getCarList()) {
+            System.out.println(c.getAmountOfRides());
+        }
+    }
+}
